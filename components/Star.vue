e519bd6389b4c76966c215d11fad19aa02c5a8f4
DutchPrince <koendegoffau12@gmail.com>
2020-12-05 17:35:17 +0100

    change

--- components/Star.vue
+++ components/Star.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="star"
+    :class="`star star-${Math.round(Math.random() * 6)}`"
     :style="{
       width,
       height: width,
@@ -71,5 +71,103 @@ export default {
     2% 35%,
     39% 35%
   );
+  &-0 {
+    &:after {
+      animation: slide 1s infinite 2s;
+    }
+  }
+  &-1 {
+    &:after {
+      animation: slide 2s infinite 3s;
+    }
+  }
+  &-2 {
+    &:after {
+      animation: slide 3s infinite 4s;
+    }
+  }
+  &-3 {
+    &:after {
+      animation: slide 4s infinite 1s;
+    }
+  }
+  &-5 {
+    &:after {
+      animation: slide 5 infinite 3s;
+    }
+  }
+  &-6 {
+    &:after {
+      animation: slide 6s infinite 4s;
+    }
+  }
+  &:after {
+    content: "";
+    top: 0;
+    transform: translateX(100%);
+    width: 100%;
+    height: 220px;
+    position: absolute;
+    z-index: 1;
+
+    /*
+  CSS Gradient - complete browser support from http://www.colorzilla.com/gradient-editor/
+  */
+    background: -moz-linear-gradient(
+      left,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 0.8) 50%,
+      rgba(128, 186, 232, 0) 99%,
+      rgba(125, 185, 232, 0) 100%
+    ); /* FF3.6+ */
+    background: -webkit-gradient(
+      linear,
+      left top,
+      right top,
+      color-stop(0%, rgba(255, 255, 255, 0)),
+      color-stop(50%, rgba(255, 255, 255, 0.8)),
+      color-stop(99%, rgba(128, 186, 232, 0)),
+      color-stop(100%, rgba(125, 185, 232, 0))
+    ); /* Chrome,Safari4+ */
+    background: -webkit-linear-gradient(
+      left,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 0.8) 50%,
+      rgba(128, 186, 232, 0) 99%,
+      rgba(125, 185, 232, 0) 100%
+    ); /* Chrome10+,Safari5.1+ */
+    background: -o-linear-gradient(
+      left,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 0.8) 50%,
+      rgba(128, 186, 232, 0) 99%,
+      rgba(125, 185, 232, 0) 100%
+    ); /* Opera 11.10+ */
+    background: -ms-linear-gradient(
+      left,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 0.8) 50%,
+      rgba(128, 186, 232, 0) 99%,
+      rgba(125, 185, 232, 0) 100%
+    ); /* IE10+ */
+    background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0) 0%,
+      rgba(255, 255, 255, 0.8) 50%,
+      rgba(128, 186, 232, 0) 99%,
+      rgba(125, 185, 232, 0) 100%
+    ); /* W3C */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00ffffff', endColorstr='#007db9e8',GradientType=1 ); /* IE6-9 */
+  }
+}
+/* animation */
+
+@keyframes slide {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(100%);
+  }
 }
 </style>
